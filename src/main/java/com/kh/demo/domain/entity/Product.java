639ffccd9dd868c562df2c738d98a8e9a77d89fc
Hyun702 <plka5824@gmail.com>
2025-05-07 17:45:08 +0900

    Add files via upload

---
+++ src/main/java/com/kh/demo/domain/entity/Product.java
@@ -0,0 +1,15 @@
+package com.kh.demo.domain.entity;
+
+import lombok.*;
+
+//@Getter   : getter 메소드 자동 생성
+//@Setter   : setter 메소드 자동 생성
+//@NoArgsConstructor : 디폴트 생성자 자동 생성
+//@RequiredArgsConstructor : final 필드 자동 생성
+//@AllArgsConstructor : 모든 멤버 필드를 매개변수로 하는 생성
+//@ToString : toString 메소드 재정의 자동생성
+//@EqualsAndHashCode : equals, hashcode 메소드 자동 생성
+@Data   // @Getter + @Setter + @Tostring + @EqualsAndHashCode + @RequiredArgsConstructor
+public class Product {
+
+}
